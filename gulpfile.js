9ec853e2477e7d852488f7e66d58a1f5cbb62efb
Jorge Sierra <jsierra.siete@gmail.com>
2017-03-04 23:39:33 +0000

    First commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
-var task = require('shell-task');
 
-gulp.task('x', function () {
-    return gulp.src('').pipe(shell.task([
-        'echo hola',
-        'echo adios'
-    ]));
-});
+gulp.task('updateGitbook', shell.task([
+    'cd docs',
+    'git init',
+    'git add .',
+    'git commit -m "First commit"',
+    'git remote add gitbook https://git.gitbook.com/ediolot/routas-expressjs.git',
+    'git pull gitbook master'
+]));
