3a2526723b75f368936ff0f93dcd3a9ac0a93886
Jorge Sierra <jsierra.siete@gmail.com>
2017-03-04 23:23:25 +0000

    Test gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,9 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var task = require('shell-task');
 
-gulp.task('default', ['build', 'deploy', 'serve']);
-
-gulp.task('build', shell.task(['npm run build']));
-
-gulp.task('deploy', shell.task(['npm run deploy']));
-gulp.task('x', shell.task(["echo hola"]));
-gulp.task('serve',  function() {
-  return gulp.src('').pipe(shell(['node hello.js']));
+gulp.task('x', function () {
+    return gulp.src('').pipe(shell.task([
+        'echo hola',
+        'echo adios'
+    ]));
 });
