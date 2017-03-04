c732991121bdf24bb3e48fb01d9f7d84d9e59c87
Kevin Díaz Marrero <alu0100880625@ull.edu.es>
2017-03-04 22:56:41 +0000

    Añadido el fichero de ejemplo de middleware y sus dependencias

---
+++ src/middleware.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var router = express.Router();
+
+
+app.set('port', (process.env.PORT || 8080));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// a middleware function with no mount path. This code is executed for every request to the router
+router.use(function (req, res, next) {
+  console.log('Time:', Date.now());
+  next();
+});
+
+// a middleware sub-stack shows request info for any type of HTTP request to the /user/:id path
+router.use('/user/:id', function(req, res, next) {
+  console.log('Request URL:', req.originalUrl);
+  next();
+}, function (req, res, next) {
+     console.log('Request Type:', req.method);
+     next();
+});
+
+// a middleware sub-stack that handles GET requests to the /user/:id path
+router.get('/user/:id', function (req, res, next) {
+  // if the user ID is 0, skip to the next router
+  if (req.params.id == 0) next('route');
+  // otherwise pass control to the next middleware function in this stack
+  else next(); //
+}, function (req, res, next) {
+   // render a regular page
+     res.render('regular.ejs', { usuario: req.params.id});
+});
+
+// handler for the /user/:id path, which renders a special page
+router.get('/user/:id', function (req, res, next) {
+  console.log(req.params.id);
+  res.render('special.ejs', { usuario: req.params.id});
+});
+
+// mount the router on the app
+app.use('/', router);
+var server = app.listen(app.get('port'), function() {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+})
