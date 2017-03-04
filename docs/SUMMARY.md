ac091f131f23c57cf267c98dd6a35a0a1d20aa87
Jorge Sierra <jsierra.siete@gmail.com>
2017-03-04 22:59:37 +0000

    Gulpfile

--- docs/SUMMARY.md
+++
@@ -1,8 +0,0 @@
-# Summary
-
-* [Introduction](README.md)
-* Routing
-    * [Routing](chapter1/routing.md)
-    * [Routing básico](chapter1/routingBasico.md)
-* [Usando Middleware](chapter2/UsandoMiddleware.md)
-* [Router API Documentation](chapter3/routerApiDocumentation.md)
---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var task = require('shell-task');
+
+gulp.task('default', ['build', 'deploy', 'serve']);
+
+gulp.task('build', shell.task(['npm run build']));
+
+gulp.task('deploy', shell.task(['npm run deploy']));
+
+gulp.task('serve',  function() {
+  return gulp.src('').pipe(shell(['node hello.js']));
+});
