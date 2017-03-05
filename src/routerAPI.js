33f47bec3a9f93031f071679439aae54b771aac7
Airam Navas <alu0100618426@ull.edu.es>
2017-03-05 01:06:18 +0000

    Añadiendo ejemplo router API

---
+++ src/routerAPI.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.send('Principal');
+});
+
+router.all('/all', function (req, res) {
+    res.send('Método all ...');
+});
+
+router.param('id', function (req, res, next, id) {
+  console.log('Invocado una sola vez');
+  next();
+});
+
+router.get('/user/:id', function (req, res, next) {
+  console.log('Este coincide');
+  next();
+});
+
+router.get('/user/:id', function (req, res) {
+  console.log('Y este tambien coincide');
+  res.end();
+});
+
+router.route('/users/:id')
+.all(function(req, res, next) {
+  res.send('Método all Route ...');
+  next();
+});
+
+router.use('/bar', function(req, res, next) {
+  res.send('Método use ...');
+  next();
+});
+
+
+app.use('/', router);
+app.listen(port, function () {
+    console.log('Example listening on port 3000!');
+});
